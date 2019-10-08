bf411953301091c485451c0c487b5c018f6bb233
Carlos Ramos <c_alfredo_r_q@hotmail.com>
2019-10-08 14:29:45 -0500

    Node web app to watch server

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const expres = require("express")
+const app = expres()
+
+const MongoConection = require("./connectionDB")
+
+app.get("/", async (req, res) => {
+    return res.send("Hola mundo!")
+})
+
+app.post("/hour", async (req, res) => {
+    let hour = {
+        hour: Math.floor(Math.random() * 24),
+        minute: Math.floor(Math.random() * 59)
+    }
+    let conection = new MongoConection();
+    await conection.inser("watch", hour)
+    return res.json(hour)
+})
+
+app.get("/hours", async (req, res) => {
+    let conection = new MongoConection();
+    let listElements = await conection.getElements("watch")
+    return res.json(listElements)
+})
+
+app.listen(3000, () => console.log("Listening port 3000"))
---
+++ connectionDB.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://carlos29:bmsgV9Umue8l2qQ5@cluster0-8hsgm.mongodb.net/BowheadHealth?retryWrites=true&w=majority";
+const uri = "mongodb://myMongoDb:27017/local"
+const dbName = "local"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+class MongoConection {
+
+  countElements(collectionName) {
+
+    return new Promise((resolve, reject) => {
+
+      client.connect( async (err) => {
+        if (err) reject (err)
+        let collection = client.db(dbName).collection(collectionName)
+        collection.count(function (err, count) {
+          client.close();
+          return err ? reject(err) : resolve(count);
+        });      
+      });
+
+    })
+
+  }
+
+  getElements(collectionName) {
+    return new Promise((resolve, reject) => {
+
+      client.connect( async (err) => {
+        if (err) reject (err)
+        let collection = client.db(dbName).collection(collectionName)
+        collection.find()
+          .toArray(function(err, data) {
+            client.close();
+            return err ? reject(err) : resolve(data);
+          });
+      });
+
+    })
+  }
+    
+  inser(collectionName, element) {
+    return new Promise((resolve, reject) => {
+
+      client.connect( async (err) => {
+        if (err) reject (err)
+        let collection = client.db(dbName,collectionName).collection(collectionName)
+        let insertedElement = collection.insertOne(element)
+          .then( response => {
+            client.close();
+            return response;
+          })
+        resolve(insertedElement)
+      });
+
+    })
+  }
+
+}
+
+module.exports = MongoConection
