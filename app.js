5085a4c628bb84a6ed74d75b464c009d39900830
Carlos Ramos <c_alfredo_r_q@hotmail.com>
2019-10-10 11:50:20 -0500

    Permitir accessos Allow-Origin

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = expres()
 
 const MongoConection = require("./connectionDB")
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.get("/", async (req, res) => {
     return res.send("Hola mundo!")
 })
